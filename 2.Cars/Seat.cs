fd9baa494920d1f147f5f59cc213f0f1bf86c638
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-08-07 10:36:24 +0300

    Add project files.

---
+++ 2.Cars/Seat.cs
@@ -0,0 +1,26 @@
+﻿namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Start()
+        {
+            return $"Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            //Grey Seat Leon
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+    }
+}
---
+++ 2.Cars/Tesla.cs
@@ -0,0 +1,30 @@
+﻿namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int Batteries { get; private set; }
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Batteries = batteries;
+        }
+        public string Start()
+        {
+            return $"Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            //Red Tesla Model 3 with 2 Batteries
+            return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Batteries} {nameof(this.Batteries)}";
+        }
+
+
+    }
+}
